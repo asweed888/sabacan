c28252ddb5e351c271b98d1733de04dc62f80313
inadati <dr.inadati@gmail.com>
2024-01-29 14:04:23 +0900

    modblock::Kitに名前を変更 (#7)

--- src/rust/modblock.rs
+++ src/rust/modblock.rs
@@ -1,7 +1,7 @@
 use crate::manifest::domain::model::entity::Manifest;
 use std::path::PathBuf;
 
-pub trait Handler<'a> {
+pub trait Kit<'a> {
     fn write_modblock(&self, manifest: &'a Manifest);
     fn main_rs_path(&self, manifest: &'a Manifest, path: &'a PathBuf);
     fn old_modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
