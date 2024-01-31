46d6a8a8ff9bc080f0e70282a92d4ff73300a16d
inadati <dr.inadati@gmail.com>
2024-01-31 13:53:51 +0900

    ひとまず全ての引数をオプションに

--- src/rust/modblock.rs
+++ src/rust/modblock.rs
@@ -3,9 +3,8 @@ use std::path::PathBuf;
 
 pub trait Handler<'a> {
     fn write_modblock(&self, manifest: &'a Manifest);
-    fn main_rs_path(&self, manifest: &'a Manifest, path: &'a PathBuf);
-    fn old_modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
-    fn modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
-    fn upstream_modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
-    fn codefile_modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
+    fn old_modblock(&self, manifest: Option<&'a Manifest>, path: Option<&'a PathBuf>);
+    fn modblock(&self, manifest: Option<&'a Manifest>, path: Option<&'a PathBuf>);
+    fn upstream_modblock(&self, manifest: Option<&'a Manifest>, path: Option<&'a PathBuf>);
+    fn codefile_modblock(&self, manifest: Option<&'a Manifest>, path: Option<&'a PathBuf>);
 }
