7abbd1d72c51542a53b5e7de4f679f4285cdf2bf
inadati <dr.inadati@gmail.com>
2024-01-31 12:56:27 +0900

    modblock::Handlerを修正 (#9)

--- src/rust/modblock.rs
+++ src/rust/modblock.rs
@@ -5,7 +5,7 @@ pub trait Handler<'a> {
     fn write_modblock(&self, manifest: &'a Manifest);
     fn main_rs_path(&self, manifest: &'a Manifest, path: &'a PathBuf);
     fn old_modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
-    fn modblock(&self);
-    fn upstream_modblock(&self);
-    fn codefile_modblock(&self);
+    fn modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
+    fn upstream_modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
+    fn codefile_modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
 }
