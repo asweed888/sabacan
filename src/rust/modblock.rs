3726e4063674fa1cf14cb509a161cbd62865b4ba
inadati <dr.inadati@gmail.com>
2024-02-01 17:00:43 +0900

    manifestは必須なのでOptionを外す

--- src/rust/modblock.rs
+++ src/rust/modblock.rs
@@ -3,8 +3,8 @@ use std::path::PathBuf;
 
 pub trait Handler<'a> {
     fn write_modblock(&self, manifest: &'a Manifest);
-    fn old_modblock(&self, manifest: Option<&'a Manifest>, path: Option<&'a PathBuf>);
-    fn modblock(&self, manifest: Option<&'a Manifest>, path: Option<&'a PathBuf>);
-    fn upstream_modblock(&self, manifest: Option<&'a Manifest>, path: Option<&'a PathBuf>);
-    fn codefile_modblock(&self, manifest: Option<&'a Manifest>, path: Option<&'a PathBuf>);
+    fn old_modblock(&self, manifest: &'a Manifest, path: Option<&'a PathBuf>);
+    fn modblock(&self, manifest: &'a Manifest, path: Option<&'a PathBuf>);
+    fn upstream_modblock(&self, manifest: &'a Manifest, path: Option<&'a PathBuf>);
+    fn codefile_modblock(&self, manifest: &'a Manifest, path: Option<&'a PathBuf>);
 }
