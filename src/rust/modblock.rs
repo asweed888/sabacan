e6b68de1a312fe1654c60b1994bc8a17ca79f41b
inadati <dr.inadati@gmail.com>
2024-02-01 19:39:46 +0900

    modblock::Handlerを微修正 (#13)

--- src/rust/modblock.rs
+++ src/rust/modblock.rs
@@ -3,8 +3,15 @@ use std::path::PathBuf;
 
 pub trait Handler<'a> {
     fn write_modblock(&self, manifest: &'a Manifest);
-    fn old_modblock(&self, manifest: &'a Manifest, path: Option<&'a PathBuf>);
-    fn modblock(&self, manifest: &'a Manifest, path: Option<&'a PathBuf>);
-    fn upstream_modblock(&self, manifest: &'a Manifest, path: Option<&'a PathBuf>);
-    fn codefile_modblock(&self, manifest: &'a Manifest, path: Option<&'a PathBuf>);
+    fn modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
+    fn upstream_modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
+    fn codefile_modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
+    fn old_modblock(&self, path: &'a PathBuf) -> &str {
+        if path.to_str().unwrap().to_string().contains("lib.rs") {
+            r"pub mod[\s\S]*//.*Automatically exported by saba\."
+        }
+        else {
+            r"mod[\s\S]*//.*Automatically exported by saba\."
+        }
+    }
 }
