8d93e5fe90c498565c6201c6dd9b538b18581a1e
inadati <dr.inadati@gmail.com>
2024-02-02 16:58:26 +0900

    write_modblockをsave_modblockに変更

--- src/rust/modblock.rs
+++ src/rust/modblock.rs
@@ -3,7 +3,7 @@ use yaml_rust::Yaml;
 use std::path::PathBuf;
 
 pub trait Handler<'a> {
-    fn write_modblock(&self, manifest: &'a Manifest) -> anyhow::Result<()>;
+    fn save_modblock(&self, manifest: &'a Manifest) -> anyhow::Result<()>;
     fn modblock(&self, manifest: &'a Manifest, path: &'a PathBuf) -> anyhow::Result<String>;
     fn upstream_modblock(&self, upstream: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str) -> anyhow::Result<()>;
     fn codefile_modblock(&self, codefile: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str) -> anyhow::Result<()>;
