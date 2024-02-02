7ae1a3ec469e1d6bbbd520d1048515feb3667fbf
inadati <dr.inadati@gmail.com>
2024-02-02 16:32:42 +0900

    modblock::Handlerの微修正 (#15)

--- src/rust/modblock.rs
+++ src/rust/modblock.rs
@@ -3,10 +3,10 @@ use yaml_rust::Yaml;
 use std::path::PathBuf;
 
 pub trait Handler<'a> {
-    fn write_modblock(&self, manifest: &'a Manifest);
-    fn modblock(&self, manifest: &'a Manifest, path: &'a PathBuf);
-    fn upstream_modblock(&self, upstream: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str);
-    fn codefile_modblock(&self, codefile: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str);
+    fn write_modblock(&self, manifest: &'a Manifest) -> anyhow::Result<()>;
+    fn modblock(&self, manifest: &'a Manifest, path: &'a PathBuf) -> anyhow::Result<String>;
+    fn upstream_modblock(&self, upstream: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str) -> anyhow::Result<()>;
+    fn codefile_modblock(&self, codefile: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str) -> anyhow::Result<()>;
     fn old_modblock(&self, path: &'a PathBuf) -> &str {
         if path.to_str().unwrap().to_string().contains("lib.rs") {
             r"pub mod[\s\S]*//.*Automatically exported by saba\."
