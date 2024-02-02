e112962dbb5cb1903496e01990785ce34ca99945
inadati <dr.inadati@gmail.com>
2024-02-02 17:19:37 +0900

    old_modblockの名前変更 (#17)

--- src/rust/modblock.rs
+++ src/rust/modblock.rs
@@ -7,7 +7,7 @@ pub trait Handler<'a> {
     fn modblock(&self, manifest: &'a Manifest, path: &'a PathBuf) -> anyhow::Result<String>;
     fn upstream_modblock(&self, upstream: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str) -> anyhow::Result<()>;
     fn codefile_modblock(&self, codefile: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str) -> anyhow::Result<()>;
-    fn old_modblock(&self, path: &'a PathBuf) -> &str {
+    fn modblock_pattern(&self, path: &'a PathBuf) -> &str {
         if path.to_str().unwrap().to_string().contains("lib.rs") {
             r"pub mod[\s\S]*//.*Automatically exported by saba\."
         }
