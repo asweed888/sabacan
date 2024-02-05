beaa76a395770a95d52117f50b2a2d77e80cb364
inadati <dr.inadati@gmail.com>
2024-02-05 09:19:03 +0900

    main_rs::genを修正

--- src/rust/main_rs.rs
+++ src/rust/main_rs.rs
@@ -16,9 +16,12 @@ pub fn path<'a>(path: &'a PathBuf) -> anyhow::Result<PathBuf> {
     Ok(fpath1)
 }
 
-pub fn gen<'a>(path: &'a PathBuf) -> anyhow::Result<()> {
-    if !path.as_path().exists() {
-        let mut file = File::create(path.to_str().unwrap())?;
+pub fn gen<'a>(path: PathBuf) -> anyhow::Result<()> {
+    let path_exists = path.as_path().exists();
+    let path = path.to_str().unwrap();
+
+    if !path_exists {
+        let mut file = File::create(path)?;
         file.write_all("".as_bytes())?;
     }
     Ok(())
