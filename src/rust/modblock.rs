b75e29b301a4da8e269ff3c0e0b5820a7a9c0073
inadati <dr.inadati@gmail.com>
2024-02-05 10:58:47 +0900

    rustのslim版の為の実装完了 (#22)

--- src/rust/modblock.rs
+++ src/rust/modblock.rs
@@ -2,13 +2,14 @@ use crate::manifest::domain::model::entity::Manifest;
 use yaml_rust::Yaml;
 use std::path::PathBuf;
 
-pub trait Handler<'a> {
+pub trait ModblockHandler<'a> {
     fn save_modblock(&self, manifest: &'a Manifest) -> anyhow::Result<()>;
     fn modblock(&self, manifest: &'a Manifest, path: &'a PathBuf) -> anyhow::Result<String>;
     fn upstream_modblock(&self, upstream: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str) -> anyhow::Result<()>;
     fn codefile_modblock(&self, codefile: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str) -> anyhow::Result<()>;
-    fn modblock_pattern(&self, path: PathBuf) -> &str {
-        if path.to_str().unwrap().to_string().contains("lib.rs") {
+    fn modblock_pattern(&self, path: &'a PathBuf) -> &str {
+        let path = path.to_str().unwrap();
+        if path.to_string().contains("lib.rs") {
             r"pub mod[\s\S]*//.*Automatically exported by saba\."
         }
         else {
