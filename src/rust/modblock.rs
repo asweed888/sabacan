3c7b6fbab637b96df97610024360799816baa9cf
inadati <dr.inadati@gmail.com>
2024-02-09 07:32:33 +0900

    upstream_modblock_with_pathを追加 (#26)

--- src/rust/modblock.rs
+++ src/rust/modblock.rs
@@ -6,6 +6,9 @@ pub trait ModblockHandler<'a> {
     fn save_modblock(&self, manifest: &'a Manifest) -> anyhow::Result<()>;
     fn modblock(&self, manifest: &'a Manifest, path: &'a PathBuf) -> anyhow::Result<String>;
     fn upstream_modblock(&self, upstream: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str) -> anyhow::Result<()>;
+    fn upstream_modblock_with_path(&self, _upstream: &Vec<Yaml>, _mod_block: &mut String, _tabs: &'a str, _path: &'a PathBuf) -> anyhow::Result<()> {
+        Ok(())
+    }
     fn codefile_modblock(&self, codefile: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str) -> anyhow::Result<()>;
     fn modblock_pattern(&self, path: &'a PathBuf) -> &str {
         let path = path.to_str().unwrap();
