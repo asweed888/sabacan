d9b7518c2c1b0ffdedfd4a439dadd590737b85b6
inadati <dr.inadati@gmail.com>
2024-02-08 19:20:22 +0900

    pathをOptionに変更

--- src/rust/modblock.rs
+++ src/rust/modblock.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 pub trait ModblockHandler<'a> {
     fn save_modblock(&self, manifest: &'a Manifest) -> anyhow::Result<()>;
     fn modblock(&self, manifest: &'a Manifest, path: &'a PathBuf) -> anyhow::Result<String>;
-    fn upstream_modblock(&self, upstream: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str, path: &'a PathBuf) -> anyhow::Result<()>;
+    fn upstream_modblock(&self, upstream: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str, path: Option<&'a PathBuf>) -> anyhow::Result<()>;
     fn codefile_modblock(&self, codefile: &Vec<Yaml>, mod_block: &mut String, tabs: &'a str) -> anyhow::Result<()>;
     fn modblock_pattern(&self, path: &'a PathBuf) -> &str {
         let path = path.to_str().unwrap();
